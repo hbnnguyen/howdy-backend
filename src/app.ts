49265adbb79b0bd07ee27036a301588e6263bffc
Hannah Nguyen <hnguyen160801@gmail.com>
2023-06-08 14:06:00 -0400

    add auth middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import { userRoutes } from "./routes/users";
 import { authRoutes } from "./routes/auth";
+import { likeDislikeRoutes } from "./routes/likedislike";
 import { ExpressError, NotFoundError } from "./expressError";
 import { PrismaClient } from "@prisma/client";
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ limit: '50000mb', extended: false }));
 
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
+app.use("/likeDislike", likeDislikeRoutes);
 
 app.get("*", (req, res) => {
   throw new NotFoundError();
@@ -32,7 +34,7 @@ app.use(function (
   res: Response,
   next: NextFunction
 ) {
-  console.log("shagua")
+  console.log("shagua");
   if (process.env.NODE_ENV !== "test") console.error(err.stack);
   const status = err.status || 500;
   const message = err.message;
---
+++ src/routes/likedislike.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+import { prisma } from '../app';
+const router = express.Router();
+
+export { router as likeDislikeRoutes };
