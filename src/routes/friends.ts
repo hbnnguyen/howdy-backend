a032927f1e29bbd405e0f7e241e0e20fd18ddea8
Hannah Nguyen <hnguyen160801@gmail.com>
2023-06-09 02:46:23 -0400

    fix bug

--- src/routes/friends.ts
+++ src/routes/friends.ts
@@ -9,98 +9,100 @@ const router = express.Router();
 
 //gets all friends
 router.get("/", ensureLoggedIn, async function (req, res, next) {
-  const userId = res.locals.user.id;
+    const userId = res.locals.user.id;
 
-  const users = await prisma.user.findMany({
-    where: {
-      NOT: {
-        id: userId
-      }
-    },
-    include: {
-      likedBy: true,
-      usersLiked: true
-    }
-  });
-
-  const matches = await asyncFilter(users, async (otherUser) => {
-    const likesDislikesOther = await prisma.likeDislike.findUnique({
-      where: {
-        fromUserId_toUserId: {
-          fromUserId: userId,
-          toUserId: otherUser.id
+    const users = await prisma.user.findMany({
+        where: {
+            NOT: {
+                id: userId
+            }
+        },
+        include: {
+            likedBy: true,
+            usersLiked: true
         }
-      }
     });
 
-    const likeDislikedByOther = await prisma.likeDislike.findUnique({
-      where: {
-        fromUserId_toUserId: {
-          fromUserId: otherUser.id,
-          toUserId: userId,
-        }
-      }
+    const matches = await asyncFilter(users, async (otherUser) => {
+        const likesDislikesOther = await prisma.likeDislike.findUnique({
+            where: {
+                fromUserId_toUserId: {
+                    fromUserId: userId,
+                    toUserId: otherUser.id
+                }
+            }
+        });
+
+        const likeDislikedByOther = await prisma.likeDislike.findUnique({
+            where: {
+                fromUserId_toUserId: {
+                    fromUserId: otherUser.id,
+                    toUserId: userId,
+                }
+            }
+        });
+
+        return (likesDislikesOther?.liked && likeDislikedByOther?.liked) || false;
     });
 
-    return (likesDislikesOther?.liked && likeDislikedByOther?.liked) || false;
-  });
+    const promises = matches.map(userToUserOutput);
+    const matchesWithPics = await Promise.all(promises);
 
-  const promises = matches.map(userToUserOutput);
-  const matchesWithPics = await Promise.all(promises);
-
-  return res.json({ matches: matchesWithPics });
+    return res.json({ matches: matchesWithPics });
 });
 
 router.get("/nextPotential", ensureLoggedIn, async function (req, res, next) {
-  const userId = res.locals.user.id;
+    const userId = res.locals.user.id;
 
-  //FIXME: make this disaster more efficient
+    //FIXME: make this disaster more efficient
 
-  const users = await prisma.user.findMany({
-    where: {
-      NOT: {
-        id: userId
-      }
-    },
-    include: {
-      likedBy: true,
-      usersLiked: true
-    }
-  });
-
-  //TODO: make one query?
-
-  const newUsers = await asyncFilter(users, async (otherUser) => {
-    const likesDislikesOther = await prisma.likeDislike.findUnique({
-      where: {
-        fromUserId_toUserId: {
-          fromUserId: userId,
-          toUserId: otherUser.id
+    const users = await prisma.user.findMany({
+        where: {
+            NOT: {
+                id: userId
+            }
+        },
+        include: {
+            likedBy: true,
+            usersLiked: true
         }
-      }
     });
 
-    const likeDislikedByOther = (await prisma.likeDislike.findUnique({
-      where: {
-        fromUserId_toUserId: {
-          fromUserId: otherUser.id,
-          toUserId: userId,
+
+
+    //TODO: make one query?
+
+    const newUsers = await asyncFilter(users, async (otherUser) => {
+        const likesDislikesOther = await prisma.likeDislike.findUnique({
+            where: {
+                fromUserId_toUserId: {
+                    fromUserId: userId,
+                    toUserId: otherUser.id
+                }
+            }
+        });
+        const likeDislikedByOther = (await prisma.likeDislike.findUnique({
+            where: {
+                fromUserId_toUserId: {
+                    fromUserId: otherUser.id,
+                    toUserId: userId,
+                }
+            }
+        }));
+
+        const disliked = !likeDislikedByOther?.liked ?? false;
+
+        if (likesDislikesOther || !disliked) {
+            return false;
         }
-      }
-    }));
 
-    const disliked = !likeDislikedByOther?.liked ?? false;
+        return true;
+    });
 
-    if (likesDislikesOther || !disliked) {
-      return false;
-    }
-
-    return true;
-  });
-
-  const potential = newUsers.length !== 0 ? userToUserOutput(newUsers[0]) : null;
-
-  return res.json({ user: potential });
+    console.log("newUsers", newUsers);
+    const potential = newUsers.length !== 0 ? await userToUserOutput(newUsers[0]) : null;
+    console.log("pot",potential);
+    return res.json({ user: potential });
 });
 
 export { router as friendRoutes };
